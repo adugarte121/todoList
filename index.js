dd1a62d256c9dd5754f4f02a126e2a6b3fa42b96
aDugarte <alejandrodugarte121@gmail.com>
2019-09-30 08:36:18 -0300

    first realease

---
+++ index.js
@@ -0,0 +1,66 @@
+//jshint esversion: 6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+var items = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+
+var currentDay = new Date();
+var options = {
+  weekday: "long",
+  month: "long",
+  day: "numeric"
+};
+var day = currentDay.toDateString("en-US", options);
+
+
+res.render("list", {today: day, newItem: items});
+});
+
+app.post("/", function(req, res){
+  var item = req.body.inputText1;
+  items.push(item);
+});
+
+/* currentDay = currentDay.getDay();
+myDay = "";
+
+switch (currentDay) {
+case 0:
+myDay = "Sunday";
+break;
+case 1:
+myDay = "Monday";
+break;
+case 2:
+myDay = "Tuesday";
+break;
+case 3:
+myDay = "Wednesday";
+break;
+case 4:
+myDay = "Thursday";
+break;
+case 5:
+myDay = "Friday";
+break;
+case 6:
+myDay = "Saturday";
+break;
+  default: console.log("Error");
+}
+*/
+
+
+
+
+app.listen(process.env.PORT || port, function(){
+  console.log("listening");
+});
